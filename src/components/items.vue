f393cdec76c6480d79e7c613d9ee9a2abfb87236
李杰 <514723443@qq.com>
2016-07-19 14:57:15 +0800

    list.vue

---
+++ src/components/items.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="items">
+    <ul>
+      <li v-for="item in items">
+        <img src="{{ item.url }}">
+        <span>{{ item.name}}</span>
+        <button @click="play"></button>
+      </li>
+    </ul>
+  </div>
+</template>
+<style>
+
+</style>
+<script type="text/ecmascript-6">
+    export default{
+        data(){
+            return{
+
+            }
+        },
+        components:{
+        }
+    }
+</script>
--- src/components/list.vue
+++ src/components/list.vue
@@ -1,9 +1,36 @@
 <template>
-    list
+  <div class="list-box">
+    <div class="back"><icon></icon></div>
+    <div class="list-items">
+      <items></items>
+    </div>
+  </div>
 </template>
 <style lang="less" rel="stylesheet/less">
-
+  .list-box{
+    position: absolute;
+    width:100%;
+    height: 100%;
+    .back{
+      position: absolute;
+      width: 100%;
+      height: 0.5rem;
+      background: #ff0000;
+    }
+    .list-items{
+      position: absolute;
+      top:0.5rem;
+      bottom:0rem;
+      width:100%;
+      background: #fff;
+    }
+  }
 </style>
 <script type="text/ecmascript-6">
-
+  import items from './items'
+  export default{
+    components:{
+      items
+    }
+  }
 </script>
