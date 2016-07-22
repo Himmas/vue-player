f52cb3dd5ce23311d0300f4e01fd504e24241f9c
kylin <396882574@qq.com>
2016-07-22 20:22:28 +0800

    lrc数据接入

--- src/components/lrc.vue
+++ src/components/lrc.vue
@@ -1,31 +1,6 @@
 <template>
    <div class="lrc-box">
-       <p>作曲 : 吉田潔</p>
-       <p>作词 : 梁旋</p>
-       <p>日月星辰悄悄</p>
-       <p>升了又落</p>
-       <p class="on">我却不知白天黑夜的存在</p>
-       <p>世界消失</p>
-       <p>有你在 我心里</p>
-       <p>真美好</p>
-       <p>有一天 想成为 你骄傲</p>
-       <p>日月星辰悄悄</p>
-       <p>升了又落</p>
-       <p>我却不知白天黑夜的存在</p>
-       <p>世界消失</p>
-       <p>有你在 我心里</p>
-       <p>真美好</p>
-       <p>有一天 想成为 你的骄傲</p>
-       <p>今夕何夕 见此良人</p>
-       <p>如月之恒为你守望</p>
-       <p>每次日落日升</p>
-       <p>我一生都在</p>
-       <p>用歌声寻觅你</p>
-       <p>为你享受着生命的悲喜</p>
-       <p>请跟我乘风而去</p>
-       <p>我一生都在用歌声寻觅你</p>
-       <p>为你享受着生命的悲喜</p>
-       <p>请跟我乘风而去</p>
+       <p v-for="item in songLrc">{{item.word}}</p>
    </div>
 </template>
 <style rel="stylesheet/less" lang="less">
@@ -52,13 +27,21 @@
         props:['songId'],
         data(){
             return{
-                songLrc:{}
+                songLrc:[]
             }
         },
         created(){
             this.$http.get(window.HOST+'/lrc?id='+this.songId).then((response) => {
-                console.log(response.data);
-                this.$set('songLrc', JSON.parse(response.data.lrc.lyric))
+                var lrcInfo = JSON.parse(response.data)
+                var lrcStr = lrcInfo.lrc.lyric
+                var lrcArr = lrcStr.split('\n')
+                lrcArr.forEach((o,i)=>{
+                    lrcArr[i] = {
+                        word: o.replace(/\[(.+)\](.+)/g,'$2'),//歌词
+                        time: o.replace(/\[(.+)\](.+)/g,'$1')//时间
+                    }
+                })
+                this.$set('songLrc', lrcArr)
             }, (response) => {
                 // error callback
             });
