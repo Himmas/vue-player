779b0d2a4c4f129c3786e84555c7bfb72a9080e6
kylin <396882574@qq.com>
2016-07-25 09:28:04 +0800

    default branch test

--- app.js
+++ app.js
@@ -11,16 +11,16 @@ app.use('/static', express.static('./static'))
 
 // app.use(bodyParser())
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }))
 
 //设置跨域访问
 app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    // res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type');
-    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Origin", "*")
+    // res.header("Access-Control-Allow-Headers", "X-Requested-With")
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type')
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS")
     res.header("X-Powered-By",' 3.2.1')
-    res.header("Content-Type", "application/json;charset=utf-8");
+    res.header("Content-Type", "application/json;charset=utf-8")
     next();
 });
 
