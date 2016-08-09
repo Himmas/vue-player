340cffa4cd9e2250db089eb10bd3065c6249a499
李杰 <514723443@qq.com>
2016-08-09 15:43:59 +0800

    热门歌单titile

--- src/components/hotList.vue
+++ src/components/hotList.vue
@@ -2,7 +2,7 @@
     <div class="hotList">
         <banner></banner>
         <div class="shop">
-            <div class="block fl">
+            <div class="block fl ml">
                 <div class="iconborder">
                     <i class="iconfont icon-fm"></i>
                 </div>
@@ -14,11 +14,20 @@
                 </div>
                 <p>每日推荐</p>
             </div>
-            <div class="block fr">
+            <div class="block fr mr">
                 <div class="iconborder">
                     <i class="iconfont icon-myPlayList"></i>
                 </div>
                 <p>我的歌单</p>
+            </div>
+        </div>
+        <div class="hot">
+            <div class="column">
+                <i class="iconfont icon-hot"></i>
+                <h1>热门歌单</h1>
+                <div class="more">
+                    更多
+                </div>
             </div>
         </div>
     </div>
@@ -33,6 +42,7 @@
         height:.6rem;
         text-align: center;
         margin-top: .1rem;
+        margin-bottom: .3rem;
         .block{
             display: inline-block;
         }
@@ -43,14 +53,40 @@
             border-radius: 50%;
         }
         .iconfont{
-            line-height:.6rem;
+            line-height:.56rem;
             color:#ee5648;
         }
         .fr{
             float:right;
         }
+        .mr{
+            margin-right:.4rem;
+        }
         .fl{
             float:left;
+        }
+        .ml{
+            margin-left:.4rem;
+        }
+    }
+    .hot{
+        border-top:1px solid #eee;
+        padding:.06rem;
+        .column{
+            position:relative;
+            height:.4rem;
+            .iconfont{
+                color:#ee5648;
+            }
+            h1{
+                display:inline-block;
+            }
+            .more{
+                color:#888;
+                display:inline-block;
+                float:right;
+                line-height:.4rem;
+            }
         }
     }
 
