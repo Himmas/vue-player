830307c46e75003336e03d982b2154c41a809e60
李杰 <514723443@qq.com>
2016-08-13 23:50:52 +0800

    我的歌单结构数据

--- src/components/myPlayList.vue
+++ src/components/myPlayList.vue
@@ -1,20 +1,42 @@
 <template>
-myplaylist
+    <div class="playlist-bd">
+        <div class="my-lists">
+            我创建的歌单
+        </div>
+        <div class="store-lists">
+            收藏的歌单
+        </div>
+    </div>
 </template>
 <style rel="stylesheet/less" lang="less">
-
+    .playlist-bd{
+        position: relative;
+        top:.5rem;
+    }
 </style>
 <script type="text/ecmascript-6">
     export default{
         data(){
             return{
-                msg:'hello vue'
+                myLists:[],
+                storeLists:[]
             }
         },
         methods:{
             getUserPlayLists(){
                 this.$http.get(window.HOST+'/userPlaylists?id=312224813').then((response)=>{
-                    console.log(JSON.parse(response.data))
+                    let _myPlayLists = JSON.parse(response.data).playlist
+                    let _myLists = []
+                    let _storeLists = []
+                    _myPlayLists.forEach(function(v){
+                        if(v.userId == 312224813){
+                            _myLists.push(v)
+                        }else{
+                            _storeLists.push(v)
+                        }
+                    })
+                    this.myLists = _myLists
+                    this.storeLists = _storeLists
                 }),(response) => {
                     // error callback
                 }
