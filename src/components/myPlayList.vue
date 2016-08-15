0a32c468463ab305e5ab2194aa6535b713098ae1
李杰 <514723443@qq.com>
2016-08-15 23:48:28 +0800

    我的歌单标题

--- src/components/myPlayList.vue
+++ src/components/myPlayList.vue
@@ -1,10 +1,24 @@
 <template>
     <div class="playlist-bd">
         <div class="my-lists">
-            <label>我创建的歌单</label>
+            <div class="lists-title">
+                <i class="iconfont" :class="myTag?'icon-bottom':'icon-right'"></i>
+                <label>我创建的歌单</label>
+                <i class="iconfont icon-vertical" ></i>
+            </div>
+            <ul class="my-items">
+                <li v-for="list in myLists"></li>
+            </ul>
         </div>
         <div class="store-lists">
-            收藏的歌单
+            <div class="lists-title">
+                <i class="iconfont" :class="myTag?'icon-bottom':'icon-right'"></i>
+                <label>收藏的歌单</label>
+                <i class="iconfont icon-vertical" ></i>
+            </div>
+            <ul class="store-items">
+                <li v-for="list in storeLists"></li>
+            </ul>
         </div>
     </div>
 </template>
@@ -12,6 +26,26 @@
     .playlist-bd{
         position: relative;
         top:.5rem;
+        .lists-title{
+            position: relative;
+            height: .4rem;
+            background-color: #eee;
+            color: #6D6D6D;
+            i{
+                line-height: .4rem;
+            }
+            label{
+                position: absolute;
+                height:100%;
+                line-height: .4rem;
+            }
+            .icon-vertical{
+                float: right;
+            }
+        }
+        .store-lists{
+            border-top: 1px solid #fff;
+        }
     }
 </style>
 <script type="text/ecmascript-6">
@@ -19,7 +53,9 @@
         data(){
             return{
                 myLists:[],
-                storeLists:[]
+                storeLists:[],
+                myTag:false,
+                storeTag:false
             }
         },
         methods:{
